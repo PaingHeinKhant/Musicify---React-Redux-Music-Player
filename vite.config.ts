7cb3179a3f366de52228b1dd8d4fc3b06d1d0ed5
paingheinkhant <paingheinkhant.mm@gmail.com>
2024-03-23 00:38:27 +0630

    vercel redux

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: "localhost",
+    port: 3000,
+  },
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: "musicify-bundle.[ext]",
+        entryFileNames: "musicify-bundle.js",
+      },
+    },
+  },
+});
